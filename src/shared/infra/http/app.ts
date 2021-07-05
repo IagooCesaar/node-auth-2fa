1e4ed672c06bfbc57d05067880607542299dfcc9
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-05 10:24:47 -0300

    criando conexão com banco de dados

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,8 +1,9 @@
 import express from 'express'
+import { createConnection } from 'typeorm';
 
+createConnection();
 const app = express();
 app.use(express.json())
-
 
 app.get('/', (req, res) => {
   return res.json({message: "Hello World"})
