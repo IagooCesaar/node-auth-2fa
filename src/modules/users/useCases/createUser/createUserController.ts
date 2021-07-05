38ce9daba2c1a8e6076cf56390a4ce169ef8b8e8
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-05 14:51:13 -0300

    createUserController

--- src/modules/users/useCases/createUser/createUserController.ts
+++ src/modules/users/useCases/createUser/createUserController.ts
@@ -1,8 +1,20 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateUserUseCase } from "./createUserUseCase";
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    return response.status(201).json({ message: "created" });
+    const { name, email, password } = request.body;
+    const createUserUseCase = container.resolve(CreateUserUseCase);
+
+    const user = await createUserUseCase.execute({
+      email,
+      name,
+      password,
+    });
+
+    return response.status(201).json(user);
   }
 }
 
