7ade50c8049f97af087113a712266b809c1d11c0
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-05 17:12:51 -0300

    find user by id

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -4,6 +4,7 @@ import { User } from "../infra/typeorm/entities/User";
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<User>;
   findByEmail(email: string): Promise<User>;
+  findById(id: string): Promise<User>;
 }
 
 export { IUsersRepository };
--- src/modules/users/repositories/in-memory/InMemoryUsersRepository.ts
+++ src/modules/users/repositories/in-memory/InMemoryUsersRepository.ts
@@ -10,6 +10,10 @@ class InMemoryUsersRepository implements IUsersRepository {
     return this.users.find((user) => user.email === email);
   }
 
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+
   async create({ email, name, password }: ICreateUserDTO): Promise<User> {
     const user = new User();
     Object.assign(user, { email, name, password });
