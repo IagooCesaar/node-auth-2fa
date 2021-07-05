16b245112cb89656257a0c72545f8e6e1e103221
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-05 14:44:15 -0300

    should be able to create a user

--- src/modules/users/useCases/createUser/createUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/createUserUseCase.spec.ts
@@ -1,9 +1,22 @@
+import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+
+import { CreateUserUseCase } from "./createUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let usersRepository: InMemoryUsersRepository;
+
 describe("createUserUseCase", () => {
   beforeEach(() => {
-    // repositoryInMemory
+    usersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
   });
 
   it("Should be able to create a new user", async () => {
-    //
+    const user = await createUserUseCase.execute({
+      email: "test@example.com",
+      name: "John Doe",
+      password: "secret",
+    });
+    expect(user).toHaveProperty("id");
   });
 });
