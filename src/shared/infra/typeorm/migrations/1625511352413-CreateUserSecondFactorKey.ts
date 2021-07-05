4ad3478c3d7d2581ab051f35b9101de631797ad8
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-05 16:03:42 -0300

    CreateUserSecondFactorKey migration

---
+++ src/shared/infra/typeorm/migrations/1625511352413-CreateUserSecondFactorKey.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUserSecondFactorKey1625511352413
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "UserSecondFactorKey",
+        columns: [
+          { name: "user_id", type: "uudi", isPrimary: true },
+          { name: "key", type: "varchar" },
+          { name: "created_at", type: "timestamp", default: "now()" },
+        ],
+        foreignKeys: [
+          {
+            name: "FK_User2FAKey_Users",
+            referencedTableName: "Users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "RESTRICT",
+            onUpdate: "RESTRICT",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("UserSecondFactorKey");
+  }
+}
