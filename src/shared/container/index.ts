57c643808c90954c782a8fa30cdc1142e692df30
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-05 16:44:31 -0300

    registro UserSecondFactorKeyRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,16 @@
 import { container } from "tsyringe";
 
+import { UserSecondFactorKeyRepository } from "@modules/users/infra/typeorm/repositories/UserSecondFactorKeyRepository";
 import { UsersRepository } from "@modules/users/infra/typeorm/repositories/UsersRepository";
+import { IUserSecondFactorKeyRepository } from "@modules/users/repositories/IUserSecondFactorKeyRepository";
 import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
+);
+
+container.registerSingleton<IUserSecondFactorKeyRepository>(
+  "UserSecondFactorKeyRepository",
+  UserSecondFactorKeyRepository
 );
