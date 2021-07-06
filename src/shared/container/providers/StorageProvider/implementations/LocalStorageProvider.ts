ae85e508d20aff8955663b3c1d2d221744687060
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-06 11:12:07 -0300

    utilizando diretorio local para testes

--- src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -8,16 +8,21 @@ import { IStorageProvider } from "../IStorageProvider";
 
 class LocalStorageProvider implements IStorageProvider {
   async save(file: string, folder: string): Promise<string> {
-    await fs.promises.rename(
-      path.resolve(upload.tmpFolder, file),
-      path.resolve(`${upload.tmpFolder}/${folder}`, file)
-    );
+    const newPath =
+      process.env.NODE_ENV === "test"
+        ? path.resolve(`${upload.tmpFolder}/test/${folder}`, file)
+        : path.resolve(`${upload.tmpFolder}/${folder}`, file);
+
+    await fs.promises.rename(path.resolve(upload.tmpFolder, file), newPath);
     await deleteFile(path.resolve(upload.tmpFolder, file));
     return file;
   }
 
   async delete(file: string, folder: string): Promise<void> {
-    const fileName = path.resolve(`${upload.tmpFolder}/${folder}`, file);
+    const fileName =
+      process.env.NODE_ENV === "test"
+        ? path.resolve(`${upload.tmpFolder}/test/${folder}`, file)
+        : path.resolve(`${upload.tmpFolder}/${folder}`, file);
     await deleteFile(fileName);
   }
 }
