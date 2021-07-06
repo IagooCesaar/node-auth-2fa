1c0dbe8d9ef4dfca1ae34a912e8080e5da5cd77e
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-06 11:04:41 -0300

    adequaçoes no teste

--- src/modules/users/useCases/generate2faKey/generate2faKeyUseCase.spec.ts
+++ src/modules/users/useCases/generate2faKey/generate2faKeyUseCase.spec.ts
@@ -1,20 +1,24 @@
 import { InMemoryUserSecondFactorKeyRepository } from "@modules/users/repositories/in-memory/InMemoryUserSecondFactorKeyRepository";
 import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { LocalStorageProvider } from "@shared/container/providers/StorageProvider/implementations/LocalStorageProvider";
 
 import { Generate2faKeyUseCase } from "./generate2faKeyUseCase";
 
 let generate2faKeyUseCase: Generate2faKeyUseCase;
 let userRepositoryInMemory: InMemoryUsersRepository;
+let storageProvider: LocalStorageProvider;
 let userSecondFactorKeyRepositoryInMemory: InMemoryUserSecondFactorKeyRepository;
 
 describe("Generate2faKeyUseCase", () => {
   beforeEach(() => {
     userRepositoryInMemory = new InMemoryUsersRepository();
+    storageProvider = new LocalStorageProvider();
     userSecondFactorKeyRepositoryInMemory =
       new InMemoryUserSecondFactorKeyRepository();
     generate2faKeyUseCase = new Generate2faKeyUseCase(
       userSecondFactorKeyRepositoryInMemory,
-      userRepositoryInMemory
+      userRepositoryInMemory,
+      storageProvider
     );
   });
 
