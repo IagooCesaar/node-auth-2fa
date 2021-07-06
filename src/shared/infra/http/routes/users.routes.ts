811cc623874a02d12601644d58afa15dbe11f181
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-06 10:34:59 -0300

    rota para geração de qrcode

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateUserController } from "@modules/users/useCases/createUser/createUserController";
+import { Generate2faKeyController } from "@modules/users/useCases/generate2faKey/generate2faKeyController";
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const generate2faKeyController = new Generate2faKeyController();
 
 usersRoutes.post("/", createUserController.handle);
+usersRoutes.post("/generate2fa", generate2faKeyController.handle);
 
 export { usersRoutes };
