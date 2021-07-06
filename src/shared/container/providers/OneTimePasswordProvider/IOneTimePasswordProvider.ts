89f6dd3768bea1d9957bc2f24e5a6945ce4d52dc
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-06 16:45:23 -0300

    configurações para OTP Provider

--- src/shared/container/providers/OneTimePasswordProvider/IOneTimePasswordProvider.ts
+++ src/shared/container/providers/OneTimePasswordProvider/IOneTimePasswordProvider.ts
@@ -1,6 +1,7 @@
 interface IOneTimePasswordProvider {
   generateBase32Key(key: string): string;
   generateToken(secret: string): string;
+  verifyToken(token: string, secret: string);
 }
 
 export { IOneTimePasswordProvider };
--- src/shared/container/providers/OneTimePasswordProvider/implementations/OTPLibProvider.ts
+++ src/shared/container/providers/OneTimePasswordProvider/implementations/OTPLibProvider.ts
@@ -3,13 +3,36 @@ import { totp } from "otplib";
 
 import { IOneTimePasswordProvider } from "../IOneTimePasswordProvider";
 
+function loadOptions() {
+  const data1 = new Date();
+  const data2 = new Date(data1.valueOf() - data1.getTimezoneOffset() * 60000);
+  const epoch = Math.floor(data2.valueOf() / 100);
+
+  console.log("totp.options", data2.toISOString(), epoch);
+
+  totp.resetOptions();
+  totp.options = {
+    digits: 6,
+    step: 30,
+    window: 5,
+    epoch,
+  };
+}
+
 class OTPLibProvider implements IOneTimePasswordProvider {
+  verifyToken(token: string, secret: string) {
+    loadOptions();
+    return totp.verify({ token, secret });
+  }
+
   generateToken(secret: string): string {
+    loadOptions();
     const token = totp.generate(secret);
     return token;
   }
 
   generateBase32Key(key: string): string {
+    loadOptions();
     return base32.encode(key);
   }
 }
