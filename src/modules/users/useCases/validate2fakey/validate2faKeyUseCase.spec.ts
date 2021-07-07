6ce516b920820d9e8be7dca0d53ae62530e0e276
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-07 17:28:03 -0300

    teste para validação de chave

---
+++ src/modules/users/useCases/validate2fakey/validate2faKeyUseCase.spec.ts
@@ -0,0 +1,51 @@
+import { InMemoryUserSecondFactorKeyRepository } from "@modules/users/repositories/in-memory/InMemoryUserSecondFactorKeyRepository";
+import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { OTPLibProvider } from "@shared/container/providers/OneTimePasswordProvider/implementations/OTPLibProvider";
+import { LocalStorageProvider } from "@shared/container/providers/StorageProvider/implementations/LocalStorageProvider";
+
+import { Generate2faKeyUseCase } from "../generate2faKey/generate2faKeyUseCase";
+import { Validate2faKeyUseCase } from "./validate2faKeyUseCase";
+
+let validate2faKeyUseCase: Validate2faKeyUseCase;
+let generate2faKeyUseCase: Generate2faKeyUseCase;
+let usersSecondFactorKeyRepository: InMemoryUserSecondFactorKeyRepository;
+let usersRepository: InMemoryUsersRepository;
+let storageProvider: LocalStorageProvider;
+let otp: OTPLibProvider;
+
+describe("validate2faKeyUseCase", () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    usersSecondFactorKeyRepository =
+      new InMemoryUserSecondFactorKeyRepository();
+    otp = new OTPLibProvider();
+    storageProvider = new LocalStorageProvider();
+
+    validate2faKeyUseCase = new Validate2faKeyUseCase(
+      usersSecondFactorKeyRepository,
+      otp
+    );
+    generate2faKeyUseCase = new Generate2faKeyUseCase(
+      usersSecondFactorKeyRepository,
+      usersRepository,
+      storageProvider,
+      otp
+    );
+  });
+  it("Should be able to create a new Key for a user", async () => {
+    const { id: user_id } = await usersRepository.create({
+      email: "john.doe@example.com",
+      name: "John Doe",
+      password: "secret",
+    });
+
+    const { key } = await generate2faKeyUseCase.execute(user_id);
+    const totp_code = otp.generateToken(key);
+
+    const response = await validate2faKeyUseCase.execute({
+      user_id,
+      totp_code,
+    });
+    expect(response.isCorrect).toBe(true);
+  });
+});
