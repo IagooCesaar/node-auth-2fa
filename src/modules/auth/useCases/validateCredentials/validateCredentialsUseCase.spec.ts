95074de737c35a579ff9067dd0a77ab91c9a4d55
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-07 18:07:18 -0300

    TDD para autenticação

---
+++ src/modules/auth/useCases/validateCredentials/validateCredentialsUseCase.spec.ts
@@ -0,0 +1,13 @@
+describe("Validate Credentials Use Case", () => {
+  it("Should be able to validate a user's email and password", () => {
+    // receber código temporário de login para aguardar autenticação 2 etapas
+  });
+
+  it("Should not be able to validate with inexistent email", () => {
+    //
+  });
+
+  it("Should not be able to validate a incorrect password", () => {
+    //
+  });
+});
---
+++ src/modules/auth/useCases/validateTwoFactorKey/validateTwoFactorKeyUseCase.spec.ts
@@ -0,0 +1,13 @@
+describe("Validate Two Factor Key Use Case", () => {
+  it("Should be able to validate a key and permit authentication", () => {
+    //
+  });
+
+  it("Should not be able to validate a incorrect key", () => {
+    //
+  });
+
+  it("Should not be able to validate with a incorrect temporary login code", () => {
+    //
+  });
+});
