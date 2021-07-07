381465f331ac9485ac2bc708fe9f71b19663f7d6
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-07 09:50:01 -0300

    change totp fpr authenticator

--- src/shared/container/providers/OneTimePasswordProvider/implementations/OTPLibProvider.ts
+++ src/shared/container/providers/OneTimePasswordProvider/implementations/OTPLibProvider.ts
@@ -1,50 +1,49 @@
+import crypto from "crypto";
 import * as base32 from "hi-base32";
-import { totp } from "otplib";
+import { authenticator } from "otplib";
 
 import { IOneTimePasswordProvider } from "../IOneTimePasswordProvider";
 
 function loadOptions() {
-  const now = Date.now();
-
   const date0 = new Date();
   const gmtMilliseconds = date0.getTimezoneOffset() * 60000;
   const regionDate = new Date(date0.valueOf() - gmtMilliseconds);
   const epoch = Math.floor(regionDate.getTime() / 1);
 
-  console.log("totp.options", regionDate.toISOString(), epoch);
-
-  totp.resetOptions();
-
-  totp.options = {
-    ...totp.options,
+  authenticator.resetOptions();
+  authenticator.options = {
+    ...authenticator.options,
     digits: 6,
     step: 30,
     window: 5,
+    // epoch: Date.now(),
     epoch,
   };
-  console.log(totp.options);
+  console.log(authenticator.options, regionDate.toISOString());
 }
 
 class OTPLibProvider implements IOneTimePasswordProvider {
   generateKeyURI(accountName: string, issuer: string, secret: string): string {
-    return totp.keyuri(accountName, issuer, secret);
+    return authenticator.keyuri(accountName, issuer, secret);
   }
 
   verifyToken(token: string, secret: string): boolean {
     loadOptions();
-    return totp.verify({ token, secret });
+    return authenticator.verify({ token, secret });
   }
 
   generateToken(secret: string): string {
     loadOptions();
-    const token = totp.generate(secret);
+    const token = authenticator.generate(secret);
 
     return token;
   }
 
   generateBase32Key(key: string): string {
     loadOptions();
-    return base32.encode(key);
+    return authenticator.encode(key);
+    // return crypto.randomBytes(32).toString("hex").substr(0, 20);
+    // return base32.encode(key);
   }
 }
 
