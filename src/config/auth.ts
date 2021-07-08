44a0c83a8bb36c2bdc7b6a735e8774a58c645995
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-08 10:48:09 -0300

    auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,10 @@
+export default {
+  secret_token: "d3acee670ad999360c2b5315b8f5b71c",
+  expires_in: "30m",
+  secret_refresh_token: "818088b95d6fe366c9bf357d2334f3fb",
+  expires_in_refresh_token: "8h",
+  expires_refresh_token_days: 30,
+
+  cache_temporary_token_prefix: "cache_temporary_token_prefix",
+  cache_temporary_token_expiration_minutes: 3,
+};
