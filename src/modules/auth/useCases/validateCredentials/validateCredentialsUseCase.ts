30408c5825bfae7901dc2427eaadde0ff9ca54d5
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-08 10:48:25 -0300

    gerando token temporario

--- src/modules/auth/useCases/validateCredentials/validateCredentialsUseCase.ts
+++ src/modules/auth/useCases/validateCredentials/validateCredentialsUseCase.ts
@@ -1,7 +1,10 @@
 import { compare } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
+import { v4 as uuidV4 } from "uuid";
 
+import auth from "@config/auth";
 import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
+import { ICacheProvider } from "@shared/container/providers/CacheProvider/ICacheProvider";
 
 import { ValidateCredentialsError } from "./validateCredentialsErrors";
 
@@ -10,14 +13,22 @@ interface IRequest {
   password: string;
 }
 
+interface IResponse {
+  temporaryToken: string;
+  expiresInMinutes: number;
+}
+
 injectable();
 class ValidateCredentialsUseCase {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject("CacheProvider")
+    private cacheProvider: ICacheProvider
   ) {}
 
-  async execute({ email, password }: IRequest): Promise<void> {
+  async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email);
     if (!user) {
       throw new ValidateCredentialsError.UserNotFound();
@@ -26,6 +37,19 @@ class ValidateCredentialsUseCase {
     if (!passwordMatch) {
       throw new ValidateCredentialsError.PasswordNotMatched();
     }
+
+    const temporaryToken = uuidV4();
+    await this.cacheProvider.set(
+      auth.cache_temporary_token_prefix,
+      temporaryToken,
+      user.id,
+      auth.cache_temporary_token_expiration_minutes * 60
+    );
+
+    return {
+      temporaryToken,
+      expiresInMinutes: auth.cache_temporary_token_expiration_minutes,
+    };
   }
 }
 
