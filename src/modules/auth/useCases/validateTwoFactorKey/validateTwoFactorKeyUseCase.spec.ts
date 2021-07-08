0be281004b9b00de13560a2ea0cfbff0afc23833
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-08 14:44:44 -0300

    importações previas

--- src/modules/auth/useCases/validateTwoFactorKey/validateTwoFactorKeyUseCase.spec.ts
+++ src/modules/auth/useCases/validateTwoFactorKey/validateTwoFactorKeyUseCase.spec.ts
@@ -1,3 +1,9 @@
+import "dotenv/config";
+
+import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "@modules/users/useCases/createUser/createUserUseCase";
+import { RedisCacheProvider } from "@shared/container/providers/CacheProvider/implementations/RedisCacheProvider";
+
 describe("Validate Two Factor Key Use Case", () => {
   it("Should be able to validate a key and permit authentication", () => {
     //
