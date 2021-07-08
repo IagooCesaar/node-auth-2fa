1c265901b6b5e24b3afde6253901de1eb56b0689
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-08 16:13:58 -0300

    novas dependencias para testes

--- src/modules/auth/useCases/validateTwoFactorKey/validateTwoFactorKeyUseCase.spec.ts
+++ src/modules/auth/useCases/validateTwoFactorKey/validateTwoFactorKeyUseCase.spec.ts
@@ -4,18 +4,24 @@ import { InMemoryUserTokensRepository } from "@modules/auth/repositories/in-memo
 import { InMemoryUserSecondFactorKeyRepository } from "@modules/users/repositories/in-memory/InMemoryUserSecondFactorKeyRepository";
 import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
 import { CreateUserUseCase } from "@modules/users/useCases/createUser/createUserUseCase";
+import { Generate2faKeyUseCase } from "@modules/users/useCases/generate2faKey/generate2faKeyUseCase";
+import { Validate2faKeyUseCase } from "@modules/users/useCases/validate2fakey/validate2faKeyUseCase";
 import { RedisCacheProvider } from "@shared/container/providers/CacheProvider/implementations/RedisCacheProvider";
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayJsDateProvider";
 import { OTPLibProvider } from "@shared/container/providers/OneTimePasswordProvider/implementations/OTPLibProvider";
+import { LocalStorageProvider } from "@shared/container/providers/StorageProvider/implementations/LocalStorageProvider";
 
 import { ValidateTwoFactorKeyUseCase } from "./validateTwoFactorKeyUseCase";
 
 let createUserUseCase: CreateUserUseCase;
+let generate2faKeyUseCase: Generate2faKeyUseCase;
 let validateTwoFactorKeyUseCase: ValidateTwoFactorKeyUseCase;
+let validate2faKeyUseCase: Validate2faKeyUseCase;
 let userSecondFactorKeyRepository: InMemoryUserSecondFactorKeyRepository;
 let usersRepository: InMemoryUsersRepository;
 let userTokensRepository: InMemoryUserTokensRepository;
 let cacheProvider: RedisCacheProvider;
+let storageProvider: LocalStorageProvider;
 let otp: OTPLibProvider;
 let dateProvider: DayjsDateProvider;
 
@@ -38,6 +44,18 @@ describe("Validate Two Factor Key Use Case", () => {
     );
 
     createUserUseCase = new CreateUserUseCase(usersRepository);
+
+    generate2faKeyUseCase = new Generate2faKeyUseCase(
+      userSecondFactorKeyRepository,
+      usersRepository,
+      storageProvider,
+      otp
+    );
+
+    validate2faKeyUseCase = new Validate2faKeyUseCase(
+      userSecondFactorKeyRepository,
+      otp
+    );
   });
 
   afterEach(() => {
