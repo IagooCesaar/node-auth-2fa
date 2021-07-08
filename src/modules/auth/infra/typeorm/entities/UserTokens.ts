90f62a84822b7a5b7ce6a69be4efda020a5c9c70
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-08 15:03:22 -0300

    UserTokens entity

---
+++ src/modules/auth/infra/typeorm/entities/UserTokens.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { User } from "@modules/users/infra/typeorm/entities/User";
+
+@Entity("UserTokens")
+class UsersTokens {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  refresh_token: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  user: User;
+
+  @Column()
+  expires_date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { UsersTokens };
