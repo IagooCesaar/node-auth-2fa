ec74769c31d7bdb41c9d68feccc8bee00cfd9280
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-08 16:08:44 -0300

    praparando para testes, before e after each

--- src/modules/auth/repositories/in-memory/InMemoryUserTokensRepository.ts
+++ src/modules/auth/repositories/in-memory/InMemoryUserTokensRepository.ts
@@ -3,7 +3,7 @@ import { UserTokens } from "@modules/auth/infra/typeorm/entities/UserTokens";
 
 import { IUserTokensRepository } from "../IUserTokensRepository";
 
-class UserTokensRepositoryInMemory implements IUserTokensRepository {
+class InMemoryUserTokensRepository implements IUserTokensRepository {
   private UserTokens: UserTokens[] = [];
 
   async create(data: ICreateUserTokenDTO): Promise<UserTokens> {
@@ -38,4 +38,4 @@ class UserTokensRepositoryInMemory implements IUserTokensRepository {
   }
 }
 
-export { UserTokensRepositoryInMemory };
+export { InMemoryUserTokensRepository };
--- src/modules/auth/useCases/validateTwoFactorKey/validateTwoFactorKeyUseCase.spec.ts
+++ src/modules/auth/useCases/validateTwoFactorKey/validateTwoFactorKeyUseCase.spec.ts
@@ -1,19 +1,58 @@
 import "dotenv/config";
 
+import { InMemoryUserTokensRepository } from "@modules/auth/repositories/in-memory/InMemoryUserTokensRepository";
+import { InMemoryUserSecondFactorKeyRepository } from "@modules/users/repositories/in-memory/InMemoryUserSecondFactorKeyRepository";
 import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
 import { CreateUserUseCase } from "@modules/users/useCases/createUser/createUserUseCase";
 import { RedisCacheProvider } from "@shared/container/providers/CacheProvider/implementations/RedisCacheProvider";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayJsDateProvider";
+import { OTPLibProvider } from "@shared/container/providers/OneTimePasswordProvider/implementations/OTPLibProvider";
+
+import { ValidateTwoFactorKeyUseCase } from "./validateTwoFactorKeyUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let validateTwoFactorKeyUseCase: ValidateTwoFactorKeyUseCase;
+let userSecondFactorKeyRepository: InMemoryUserSecondFactorKeyRepository;
+let usersRepository: InMemoryUsersRepository;
+let userTokensRepository: InMemoryUserTokensRepository;
+let cacheProvider: RedisCacheProvider;
+let otp: OTPLibProvider;
+let dateProvider: DayjsDateProvider;
 
 describe("Validate Two Factor Key Use Case", () => {
-  it("Should be able to validate a key and permit authentication", () => {
+  beforeEach(() => {
+    userSecondFactorKeyRepository = new InMemoryUserSecondFactorKeyRepository();
+    usersRepository = new InMemoryUsersRepository();
+    userTokensRepository = new InMemoryUserTokensRepository();
+    cacheProvider = new RedisCacheProvider();
+    otp = new OTPLibProvider();
+    dateProvider = new DayjsDateProvider();
+
+    validateTwoFactorKeyUseCase = new ValidateTwoFactorKeyUseCase(
+      userSecondFactorKeyRepository,
+      usersRepository,
+      userTokensRepository,
+      cacheProvider,
+      otp,
+      dateProvider
+    );
+
+    createUserUseCase = new CreateUserUseCase(usersRepository);
+  });
+
+  afterEach(() => {
+    cacheProvider.disconnect();
+  });
+
+  it("Should be able to validate a key and permit authentication", async () => {
     //
   });
 
-  it("Should not be able to validate a incorrect key", () => {
-    //
-  });
+  // it("Should not be able to validate a incorrect key", async () => {
+  //   //
+  // });
 
-  it("Should not be able to validate with a incorrect temporary login code", () => {
-    //
-  });
+  // it("Should not be able to validate with a incorrect temporary login code", async () => {
+  //   //
+  // });
 });
