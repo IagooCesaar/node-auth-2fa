49a103203cbf9766204be646875b2542d187e6f6
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-08 14:00:56 -0300

    ajuste para aguar atraso criaçaõ classe devido conexão redis

--- src/shared/container/providers/CacheProvider/index.ts
+++ src/shared/container/providers/CacheProvider/index.ts
@@ -1,9 +1,9 @@
-import { container } from "tsyringe";
+import { container, delay } from "tsyringe";
 
 import { ICacheProvider } from "./ICacheProvider";
 import { RedisCacheProvider } from "./implementations/RedisCacheProvider";
 
 container.registerSingleton<ICacheProvider>(
   "CacheProvider",
-  RedisCacheProvider
+  delay(() => RedisCacheProvider)
 );
