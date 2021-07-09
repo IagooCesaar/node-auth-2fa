9ba599c7ba7ce09d0d4e906bfb7d4b10713023ef
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-09 09:45:35 -0300

    profileUserUseCase

---
+++ src/modules/users/useCases/profileUser/profileUserUseCase.ts
@@ -0,0 +1,20 @@
+import { inject, injectable } from "tsyringe";
+
+import { IUserResponseDTO } from "@modules/users/dtos/IUserResponseDTO";
+import { UserMap } from "@modules/users/mappers/UserMap";
+import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
+
+@injectable()
+class ProfileUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute(user_id: string): Promise<IUserResponseDTO> {
+    const user = await this.usersRepository.findById(user_id);
+    return UserMap.toDTO(user);
+  }
+}
+
+export { ProfileUserUseCase };
