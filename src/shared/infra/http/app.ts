c7bbde22a1b369baed9c9ab360c629780f1a2a5d
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-09 10:34:14 -0300

    rota para coverage report

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,7 +3,8 @@ import "reflect-metadata";
 import "dotenv/config";
 import "@shared/container";
 
-import express from "express";
+import express, { Request, Response } from "express";
+import path from "path";
 import { createConnection } from "typeorm";
 
 import upload from "@config/upload";
@@ -16,6 +17,27 @@ const app = express();
 app.use(express.json());
 
 app.use("/users/qrcode", express.static(`${upload.tmpFolder}/qrcode`));
+
+app.use(
+  "/api/coverage",
+  express.static(
+    path.resolve(__dirname, "..", "..", "..", "..", "coverage", "lcov-report")
+  )
+);
+app.use("/api/coverage", (request: Request, response: Response) => {
+  return response.sendFile(
+    path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "..",
+      "coverage",
+      "lcov-report",
+      "index.html"
+    )
+  );
+});
 
 app.get("/", (req, res) => {
   return res.json({ message: "Hello World" });
