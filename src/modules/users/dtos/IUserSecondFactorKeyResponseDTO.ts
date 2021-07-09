5a17a6f7a416cae04d2c4e142e4203719c85f082
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-09 11:54:44 -0300

    ajustes para segurança

--- src/modules/users/dtos/IUserSecondFactorKeyResponseDTO.ts
+++ src/modules/users/dtos/IUserSecondFactorKeyResponseDTO.ts
@@ -1,9 +1,4 @@
 interface IUserSecondFactorKeyResponseDTO {
-  user_id: string;
-  key: string;
-  created_at: Date;
-  validated: boolean;
-  validated_at?: Date;
   qrcode_url: () => string;
 }
 
--- src/modules/users/mappers/UserSecondFactorKeyMap.ts
+++ src/modules/users/mappers/UserSecondFactorKeyMap.ts
@@ -5,21 +5,10 @@ import { UserSecondFactorKey } from "../infra/typeorm/entities/UserSecondFactorK
 
 class UserSecondFactorKeyMap {
   static toDTO({
-    created_at,
-    key,
     qrcode_url,
-    user,
-    user_id,
-    validated,
-    validated_at,
   }: UserSecondFactorKey): IUserSecondFactorKeyResponseDTO {
     const key2fa = classToClass({
-      created_at,
-      key,
       qrcode_url,
-      user_id,
-      validated,
-      validated_at,
     });
     return key2fa;
   }
