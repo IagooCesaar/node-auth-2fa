d85423dbd01ecd7ee6ae6581b4afa55d4cbd8b8a
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-09 09:41:03 -0300

    middleware ensureAuthenticated

---
+++ src/shared/errors/ensureAuthenticatedError.ts
@@ -0,0 +1,17 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable @typescript-eslint/no-namespace */
+import { AppError } from "@shared/errors/appError";
+
+export namespace EnsureAuthenticatedError {
+  export class TokenMissing extends AppError {
+    constructor() {
+      super("EnsureAuthenticatedError.TokenMissing", "Token missing", 401);
+    }
+  }
+
+  export class InvalidToken extends AppError {
+    constructor() {
+      super("EnsureAuthenticatedError.InvalidToken", "Invalid token", 401);
+    }
+  }
+}
---
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import auth from "@config/auth";
+import { EnsureAuthenticatedError } from "@shared/errors/ensureAuthenticatedError";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  const authHeader = request.headers.authorization;
+  if (!authHeader) {
+    throw new EnsureAuthenticatedError.TokenMissing();
+  }
+  const [_, token] = authHeader.split(" "); // Bearer Token
+  try {
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
+    request.user = {
+      id: user_id,
+    };
+  } catch (error) {
+    throw new EnsureAuthenticatedError.InvalidToken();
+  }
+}
