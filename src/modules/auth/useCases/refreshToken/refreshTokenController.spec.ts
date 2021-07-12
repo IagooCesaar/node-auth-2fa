7ff114393b86f3c931fb8850c7671d7eedf2f101
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-12 11:48:33 -0300

    iniciando testes refresh Token Controller

---
+++ src/modules/auth/useCases/refreshToken/refreshTokenController.spec.ts
@@ -0,0 +1,19 @@
+import request from "supertest";
+import { Connection } from "typeorm";
+
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
+
+let connection: Connection;
+
+describe("Refresh Token Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+});
