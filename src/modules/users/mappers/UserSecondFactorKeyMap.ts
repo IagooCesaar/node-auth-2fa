dd0025ed5cbacd21c729f95c8568454cc3aedc88
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-07-14 10:34:06 -0300

    correção url retorno

--- src/modules/users/mappers/UserSecondFactorKeyMap.ts
+++ src/modules/users/mappers/UserSecondFactorKeyMap.ts
@@ -6,9 +6,11 @@ import { UserSecondFactorKey } from "../infra/typeorm/entities/UserSecondFactorK
 class UserSecondFactorKeyMap {
   static toDTO({
     qrcode_url,
+    user_id,
   }: UserSecondFactorKey): IUserSecondFactorKeyResponseDTO {
     const key2fa = classToClass({
       qrcode_url,
+      user_id,
     });
     return key2fa;
   }
